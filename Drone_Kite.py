907a74180c7f50f2e79d39b38b071b3b9e4ef8db
joseph la delfa <cafeciaojoe>
2025-07-04 15:29:06 +0200

    logging well!

--- Drone_Kite.py
+++ Drone_Kite.py
@@ -10,6 +10,8 @@ from cflib.crazyflie.syncLogger import SyncLogger
 # URI to the Crazyflie to connect to
 uri = 'radio://0/30/2M/A0A0A0A0AA'
 
+position = [0,0,0]
+
 # Only output errors from the logging framework
 logging.basicConfig(level=logging.ERROR)
 
@@ -21,8 +23,8 @@ def simple_log_async(scf, logconf):
     cf.log.add_config(logconf)
     logconf.data_received_cb.add_callback(log_stab_callback)
     logconf.start()
-    time.sleep(5)
-    logconf.stop()
+    # time.sleep(5)
+    # logconf.stop()
 
 if __name__ == '__main__':
     # Initialize the low-level drivers
@@ -37,10 +39,24 @@ if __name__ == '__main__':
     lg_stab.add_variable('stateEstimate.pitch', 'float')
     lg_stab.add_variable('stateEstimate.yaw', 'float')
 
-    def take_off(cf):
-        
-        pass
+    def take_off(cf, height):
+        take_off_time = 1.0
+        sleep_time = 0.1
+        steps = int(take_off_time / sleep_time)
+        vz = height / take_off_time
+
+        print(f'take off at {height}')
+
+        for i in range(steps):
+            cf.commander.send_velocity_world_setpoint(0, 0, vz, 0)
+            time.sleep(sleep_time)
 
     with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
-        take_off(scf)
         simple_log_async(scf, lg_stab)
+        time.sleep(10)
+        #take_off(scf)
+        commander = scf.cf.high_level_commander()
+        scf.commander.send_position_setpoint(position[0],
+                                            position[1],
+                                            position[2],
+                                            position[3])
