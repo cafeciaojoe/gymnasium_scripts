696c03f19cfdbd4676e6e9d8d45db597827fafd9
joseph la delfa <cafeciaojoe>
2025-07-05 21:33:30 +0200

    it is kind of working, i think it needs to look at a different control funciton like one that only controls velocity and se it t 0 and allow level change.

--- Drone_Kite.py
+++ Drone_Kite.py
@@ -8,21 +8,44 @@ from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.crazyflie.syncLogger import SyncLogger
 
 # URI to the Crazyflie to connect to
-uri = 'radio://0/30/2M/A0A0A0A0AA'
+uri = 'radio://0/30/2M/BADC0DE012'
 
 position = [0,0,0]
+orientation = [0,0,0]  # roll, pitch, yaw
+
+target_position = [0]
 
 # Only output errors from the logging framework
 logging.basicConfig(level=logging.ERROR)
 
 def log_stab_callback(timestamp, data, logconf):
-    print('[%d][%s]: %s' % (timestamp, logconf.name, data))
+    #print('[%d][%s]: %s' % (timestamp, logconf.name, data))
+    position[0] = data['stateEstimate.x']
+    position[1] = data['stateEstimate.y']
+    position[2] = data['stateEstimate.z']
+    orientation[0] = data['stateEstimate.roll']
+    orientation[1] = data['stateEstimate.pitch']
+    orientation[2] = data['stateEstimate.yaw']
 
-def simple_log_async(scf, logconf):
+    # print(f"Position: {position}")
+    # print(f"Orientation: {orientation}")
+
+def log_stab_callback_2(timestamp, data, logconf):
+    #print('[%d][%s]: %s' % (timestamp, logconf.name, data))
+
+    target_position[0] = data['ctrltarget.x']
+    # print(f"Position: {position}")
+    # print(f"Orientation: {orientation}")
+
+def simple_log_async(scf, logconf_a, logconf_b):
     cf = scf.cf
-    cf.log.add_config(logconf)
-    logconf.data_received_cb.add_callback(log_stab_callback)
-    logconf.start()
+    cf.log.add_config(logconf_a)
+    logconf_a.data_received_cb.add_callback(log_stab_callback)
+    logconf_a.start()
+
+    cf.log.add_config(logconf_b)
+    logconf_b.data_received_cb.add_callback(log_stab_callback_2)
+    logconf_b.start()
     # time.sleep(5)
     # logconf.stop()
 
@@ -30,7 +53,7 @@ if __name__ == '__main__':
     # Initialize the low-level drivers
     cflib.crtp.init_drivers()
 
-    lg_stab = LogConfig(name='stateEstimate', period_in_ms=1000)
+    lg_stab = LogConfig(name='stateEstimate', period_in_ms=10)
     lg_stab.add_variable('stateEstimate.x', 'float')
     lg_stab.add_variable('stateEstimate.y', 'float')
     lg_stab.add_variable('stateEstimate.z', 'float')
@@ -39,24 +62,23 @@ if __name__ == '__main__':
     lg_stab.add_variable('stateEstimate.pitch', 'float')
     lg_stab.add_variable('stateEstimate.yaw', 'float')
 
-    def take_off(cf, height):
-        take_off_time = 1.0
-        sleep_time = 0.1
-        steps = int(take_off_time / sleep_time)
-        vz = height / take_off_time
-
-        print(f'take off at {height}')
-
-        for i in range(steps):
-            cf.commander.send_velocity_world_setpoint(0, 0, vz, 0)
-            time.sleep(sleep_time)
+    lg_stab_2 = LogConfig(name='ctrltarget', period_in_ms=10)
+    lg_stab_2.add_variable('ctrltarget.x', 'float')
 
     with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
-        simple_log_async(scf, lg_stab)
-        time.sleep(10)
+        simple_log_async(scf, lg_stab, lg_stab_2)
+        time.sleep(1)
+        #exit()
         #take_off(scf)
-        commander = scf.cf.high_level_commander()
-        scf.commander.send_position_setpoint(position[0],
-                                            position[1],
-                                            position[2],
-                                            position[3])
+        scf.cf.platform.send_arming_request(True)
+        time.sleep(1)
+        
+        scf.cf.high_level_commander.takeoff(.5, 1)
+        time.sleep(2.5)
+        while True:
+            #scf.cf.high_level_commander.go_to(position[0], position[1], position[2], 0, 1, relative=False)
+            scf.cf.commander.send_position_setpoint(position[0], position[1], position[2],0)
+            print(f'a_x = {position[0]}, x_t = {target_position[0]}')
+            time.sleep(1.1)
+        
+        
